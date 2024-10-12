f8fae4c128847c543e4e297c2fd0315db7f7afb8
陳建智 <rocky@chenjianzhideMac-mini.local>
2024-10-12 19:08:06 +0800

    callback_login_modify

--- src/routes/api/auth/seller/login/+server.ts
+++ src/routes/api/auth/seller/login/+server.ts
@@ -3,10 +3,14 @@
 import type { RequestHandler } from "@sveltejs/kit";
 import { getOIDCAuthorizationUrl } from "$lib/server/auth";
 import { redirect } from "@sveltejs/kit";
+import { base } from "$app/paths";
 
-export const GET: RequestHandler = async ({ locals }) => {
+export const GET: RequestHandler = async ({ url, locals, request }) => {
 	// 生成回调 URI
-	const redirectURI = `${import.meta.env.VITE_BASE_URL}/api/auth/seller/callback`;
+	//const redirectURI = `${import.meta.env.VITE_BASE_URL}/api/auth/seller/callback`;
+
+	const referer = request.headers.get("referer");
+	const redirectURI = `${(referer ? new URL(referer) : url).origin}${base}/api/auth/seller/callback`;
 
 	// 获取授权 URL
 	const authorizationUrl = await getOIDCAuthorizationUrl(
