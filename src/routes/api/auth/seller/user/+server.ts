9ba5f5dc88107d434d7cd348850cb24dd0ad2fcd
ice91 <chen.rocky@gmail.com>
2024-10-21 14:44:27 +0800

    https_fix

--- src/routes/api/auth/seller/user/+server.ts
+++ src/routes/api/auth/seller/user/+server.ts
@@ -1,8 +1,8 @@
 // src/routes/api/auth/seller/user/+server.ts
 
 import type { RequestHandler } from "@sveltejs/kit";
-//import jwt from "jsonwebtoken";
-import { verify } from "jsonwebtoken";
+import pkg from "jsonwebtoken";
+const { verify } = pkg;
 import { collections } from "$lib/server/database";
 import { env } from "$env/dynamic/private";
 import { ObjectId } from "mongodb";
